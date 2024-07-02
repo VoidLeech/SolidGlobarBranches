241a17989e98a7d6a78eb6bdaa9e12265a9a8c62
VoidLeech <ian.gerritse@gmail.com>
2024-07-02 17:10:17 +0200

    fix: amber not becoming glowing

---
+++ src/main/java/com/github/voidleech/solidglobarbranches/mixin/AmberBlockGlowingMixin.java
@@ -0,0 +1,20 @@
+package com.github.voidleech.solidglobarbranches.mixin;
+
+import net.mcreator.snifferent.block.AmberGlowingBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(AmberGlowingBlock.class)
+public class AmberBlockGlowingMixin extends Block {
+    public AmberBlockGlowingMixin(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public int getLightBlock(BlockState state, BlockGetter world, BlockPos pos){
+        return 0;
+    }
+}
---
+++ src/main/java/com/github/voidleech/solidglobarbranches/mixin/AmberBlockMixin.java
@@ -0,0 +1,29 @@
+package com.github.voidleech.solidglobarbranches.mixin;
+
+import net.mcreator.snifferent.block.AmberBlockBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(AmberBlockBlock.class)
+public class AmberBlockMixin extends Block {
+    public AmberBlockMixin(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Inject(method = "onPlace", at = @At(value = "INVOKE", target = "Lnet/mcreator/snifferent/procedures/AmberGlowingUpdateTickProcedure;execute(Lnet/minecraft/world/level/LevelAccessor;DDD)V", shift = At.Shift.BEFORE), cancellable = true)
+    private void sgb$cancelImmediateExecute(BlockState state, Level level, BlockPos pos, BlockState old, boolean b, CallbackInfo ci){
+        ci.cancel();
+    }
+
+    @Override
+    public int getLightBlock(BlockState state, BlockGetter world, BlockPos pos){
+        return 0;
+    }
+}
