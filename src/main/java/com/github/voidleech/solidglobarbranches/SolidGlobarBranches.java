1593e9b2db0091aa701b3af5152e79ec5985a7f9
VoidLeech <ian.gerritse@gmail.com>
2024-07-10 19:32:57 +0200

    defeat: minimal reproducer of modernfix incompat
    What's so special about snifferent's models specifically

--- src/main/java/com/github/voidleech/solidglobarbranches/SolidGlobarBranches.java
+++ src/main/java/com/github/voidleech/solidglobarbranches/SolidGlobarBranches.java
@@ -1,52 +1,15 @@
 package com.github.voidleech.solidglobarbranches;
 
-import com.github.voidleech.solidglobarbranches.registry.SGBComposting;
-import com.github.voidleech.solidglobarbranches.registry.SGBFuel;
-import com.mojang.logging.LogUtils;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
 
 @Mod(SolidGlobarBranches.MOD_ID)
 public class SolidGlobarBranches
 {
     public static final String MOD_ID = "solidglobarbranches";
-    private static final Logger LOGGER = LogUtils.getLogger();
 
     public SolidGlobarBranches()
     {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        modEventBus.addListener(this::commonSetup);
-
         MinecraftForge.EVENT_BUS.register(this);
-        MinecraftForge.EVENT_BUS.addListener(SGBFuel::addFuels);
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
-        SGBComposting.addCompostables();
-    }
-
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
-
-    }
-
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-
-        }
     }
 }
